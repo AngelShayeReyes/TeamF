6b519319daa86de3a0884ec633beaa29a5b00b92
Angel Reyes <angelshaye.reyes@kainos.com>
2021-08-12 11:48:28 +0100

    add minimal use case 1

--- app/routes.js
+++ app/routes.js
@@ -7,4 +7,24 @@ router.get('/list-employees', async (req, res) => {
     res.render('list-employees', { employees: await employeedata.getEmployees() } ) 
 });
 
+router.get('/addemployee', async(req, res)=>{ 
+    res.render('newemployeeform', { employees: await employeedata.getEmployees() } ); 
+  });
+
+  
+  router.post('/addemployee', async(req, res) => { 
+    var employee = req.body 
+    // validate here 
+    var employee_name = req.body.employee_name; 
+    if(employee_name){ 
+    
+      let insertedKey = await employeedata.addEmployee(req.body) 
+      res.render('list-employees', { employees: await employeedata.getEmployees()} ) 
+  } else {
+      res.locals.errormessage = "Name needed" 
+      res.render('newemployeeform', req.body ) 
+    }
+  })
+
+  
 module.exports = router
