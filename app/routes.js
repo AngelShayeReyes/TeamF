200fb09aad6941b3e761fc89ae98968963bd9f45
SamuelChorvat <36892499+SamuelChorvat@users.noreply.github.com>
2021-08-12 12:03:24 +0100

    add working version of user case 2

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const employeedata = require('./employeedata.js')
+const departmentdata = require('./departmentdata.js')
 
 // Add your routes here - above the module.exports line
 router.get('/list-employees', async (req, res) => { 
     res.render('list-employees', { employees: await employeedata.getEmployees() } ) 
+});
+
+router.get('/per-department/:department', async (req, res) => {
+    res.render('per-department', { departmentEmployees: await departmentdata.getEmployeesInDepartment(req.params.department), departmentName: req.params.department} ); 
 });
 
 module.exports = router
