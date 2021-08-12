91c916a3b32ac53109b4a9c37c0064ccb1b7999c
Angel Reyes <angelshaye.reyes@kainos.com>
2021-08-12 10:32:05 +0100

    add minimal proof of concept

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const employeedata = require('./employeedata.js')
 
 // Add your routes here - above the module.exports line
+router.get('/list-employees', async (req, res) => { 
+    res.render('list-employees', { employees: await employeedata.getEmployees() } ) 
+});
 
 module.exports = router
